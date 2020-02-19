c2e2af73143b3c34cb6f8451b0357562f95c8c8a
Manav Punjabi <manavpunjabi26@gmail.com>
2020-02-19 16:17:49 +0530

    final commit before deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect to MongoDB
 connectDB();
@@ -9,14 +10,26 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the Contact Keeper API" })
+app.get(
+  "/",
+  (req, res) => {}
+  // res.json({ msg: "Welcome to the Contact Keeper API" })
 );
 
 // defining routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
